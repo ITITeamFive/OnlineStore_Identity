f640c7ec8f38f2d5bba0e83a77c248ba8eef7611
Sherif <Sherif_kotb122@yahoo.com>
2021-04-14 16:35:36 +0200

    Merging Crud operation and Cards

--- OnlineStore_Identity/Controllers/ProductsController.cs
+++ OnlineStore_Identity/Controllers/ProductsController.cs
@@ -85,7 +85,30 @@ namespace OnlineStore_Identity.Controllers
             //ViewBag.Products = products.Value;
             //List<Product> x = products.Value;
             //return PartialView(x);
-            return PartialView();
+            ProductBillVM productBillVM = new ProductBillVM();
+
+            HttpResponseMessage response = client.GetAsync("http://shirleyomda-001-site1.etempurl.com/odata/Products").Result;
+            string Result = response.Content.ReadAsStringAsync().Result;
+            RootObject<Product> products = JsonConvert.DeserializeObject<RootObject<Product>>(Result);
+            productBillVM.products = products.Value;
+
+            HttpResponseMessage response2 = client.GetAsync("http://shirleyomda-001-site1.etempurl.com/odata/Bills").Result;
+            string Result2 = response2.Content.ReadAsStringAsync().Result;
+            RootObject<Bill> Bills = JsonConvert.DeserializeObject<RootObject<Bill>>(Result2);
+            IEnumerable<Bill> myBill = Bills.Value.Where(b => Convert.ToDateTime(b.billDate) == DateTime.Now.Date);
+
+            foreach (Bill bill in myBill)
+            {
+                productBillVM.todayProfit += (double)bill.billTotal;
+            }
+
+            IEnumerable<Bill> allBills = Bills.Value;
+            foreach (Bill bill in allBills)
+            {
+                productBillVM.allProfit += (double)bill.billTotal;
+            }
+            //return View(productBillVM);
+            return PartialView(productBillVM);
         }
 
         // GET: OnlineStore_Identity/AddOrEdit(Insert)
