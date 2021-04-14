cb2b0867d6755386880e3c3e665aa182855b8223
Antonius Atef Ghaly <antonius.a.ghaly@gmail.com>
2021-04-15 00:04:15 +0200

    chart sales per day

--- OnlineStore_Identity/ViewModels/ProductBillVM.cs
+++ OnlineStore_Identity/ViewModels/ProductBillVM.cs
@@ -12,11 +12,10 @@ namespace OnlineStore_Identity.ViewModels
         //public List<Product> products = new List<Product>();
         public IEnumerable<Product> products;
         public IEnumerable<Bill> bills;
-
         //public Bill bills = new Bill();
-
         public double todayProfit=0;
-
         public double allProfit=0;
+        public double[] lastMonthProfit=new double[31];
+        public double[] lastYearProfit = new double[12];
     }
 }
